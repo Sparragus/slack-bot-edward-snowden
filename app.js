ea294e202bfc7f332631e444b70ea8f2e6503b78
Richard Kaufman <richardbkaufman@gmail.com>
2015-05-27 18:54:35 -0500

    Do not warn user all the time, only after 10 seconds since last warning.

--- app.js
+++ app.js
@@ -34,25 +34,50 @@ slack.on('message', function (message) {
   }
 });
 
-// Warn a user if he/she is typing on channel #anonymous
-slack.on('userTyping', function(user, channel) {
-  // If the user is writing me, it's okay.
-  if (channel.is_im) {
-    return;
-  }
+var onUserTyping = (function() {
+  var userPreviousWarning = {};
+  return function onUserTyping (user, channel) {
+    // If the user is writing me, it's okay.
+    if (channel.is_im) {
+      return;
+    }
 
-  // If the user is typing on channel #anonymous, warn him/her
-  if (channel.is_channel) {
-    // open a DM channel and send user a message
-    slack.openDM(user.id, function(dmChannel) {
-      var dm = slack.getDMByID(dmChannel.channel.id);
-      var message = "You're directly writing on the #anonymous channel. This will expose you. If you want to send an anonymous message, send it to me and I'll forward it to #anonymous.";
+    // If the user is typing on channel #anonymous, warn him/her
+    if (channel.is_channel) {
+      // do not warn user if less than N seconds have passed since last warning
+      var secondsThreshold = 10;
+      var timeElapsedInSeconds = 0;
 
-      dm.send(message);
-      dm.close();
-    });
-  }
-});
+      // check if the user is in our records. if so, how much time has passed since the last warning?
+      if (userPreviousWarning[user.id]) {
+        timeElapsedInSeconds = (Date.now() - userPreviousWarning[user.id]) / 1000;
+      }
+      // if user is not on our records, add him/her
+      else {
+        userPreviousWarning[user.id] = Date.now();
+        timeElapsedInSeconds = secondsThreshold + 1;
+      }
+
+      // if not enough time has passed, return.
+      if ( timeElapsedInSeconds < secondsThreshold ) {
+        return;
+      }
+
+      // open a DM channel and send user a message
+      slack.openDM(user.id, function(dmChannel) {
+        var dm = slack.getDMByID(dmChannel.channel.id);
+        var message = "You're directly writing on the #anonymous channel. This will expose you. If you want to send an anonymous message, send it to me and I'll forward it to #anonymous.";
+
+        dm.send(message);
+        dm.close();
+
+        userPreviousWarning[user.id] = Date.now();
+      });
+    }
+  };
+})();
+
+slack.on('userTyping', onUserTyping);
 
 slack.on('error', function (error) {
   console.error('Error:', error);
