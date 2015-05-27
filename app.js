eb106877fbd3a1b510e7a2a4ba03e378878b0278
Richard Kaufman <richardbkaufman@gmail.com>
2015-05-27 17:33:51 -0500

    Bot receives DMs from users and forwards them to #anonymous.

--- app.js
+++ app.js
@@ -4,8 +4,47 @@ var token = process.env.SLACK_API_TOKEN;
 var autoReconnect = true;
 var autoMark = true;
 
-slack = new Slack(token, autoReconnect, autoMark);
+var slack = new Slack(token, autoReconnect, autoMark);
 
 slack.on('open', function() {
   console.log('Welcome to Slack. You are @' + slack.self.name + ' of ' + slack.team.name);
 });
+
+slack.on('message', function (message) {
+  // if message is from myself, ignore
+  
+
+  var channelGroupOrDM = slack.getChannelGroupOrDMByID(message.channel);
+  var text = message.text;
+
+  // if message is a DM from a user...
+  if (channelGroupOrDM.is_im) {
+    var anonymousChannel = slack.getChannelByName('anonymous');
+    var anonymousUser = randomUsername();
+    var someColor = randomHexColor();
+    var anonymousIcon = 'http://placehold.it/80/' + someColor + '/'+ someColor;
+
+    var response = {
+      text: text,
+      username: anonymousUser,
+      icon_url: anonymousIcon
+    };
+
+    // post message to #anonymous
+    anonymousChannel.postMessage(response);
+  }
+});
+
+slack.on('error', function (error) {
+  console.error('Error:', error);
+});
+
+slack.login();
+
+function randomUsername () {
+  return Math.random().toString(36).substring(2);
+}
+
+function randomHexColor() {
+  return Math.random().toString(16).substring(2, 2+6);
+}
