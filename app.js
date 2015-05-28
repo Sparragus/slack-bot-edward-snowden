a0e8901364f923e8eaa1a4c065d1df9187edc589
Richard Kaufman <richardbkaufman@gmail.com>
2015-05-27 23:15:04 -0500

    Subscribe users to #anonymous conversation. After a period of inactivity, a user gets unsubscribed.

--- app.js
+++ app.js
@@ -10,27 +10,106 @@ slack.on('open', function() {
   console.log('Welcome to Slack. You are @' + slack.self.name + ' of ' + slack.team.name);
 });
 
+// TODO: This obviously violates anonimity. Fix later.
+var userActivityOnChannel = {}; // {user:time, user:time, ...}
+var userSecretUsername = {}; // {user:secretUser}
+
+function onMessageFromChannel (message) {
+  var notifyUsers = [];
+
+  var users = Object.keys(userActivityOnChannel);
+  users.forEach(function(user) {
+    var lastSeen = userActivityOnChannel[user];
+
+    // After N minutes of inactivity, do not notify user and remove him from this list
+    var timeElapsed = (Date.now() - lastSeen);
+    var timeThreshold = 1.5 * 60 * 1000; // ten minutes
+    if (timeElapsed > timeThreshold) {
+      delete userActivityOnChannel[user];
+      return;
+    }
+
+    // Do not notify sender of the message. Also, remove him from the list.
+    if (message.username && message.username === userSecretUsername[user]) {
+      delete userSecretUsername[user];
+      return;
+    }
+
+    notifyUsers.push(user);
+  });
+
+  notifyUsers.forEach(function(user) {
+    var userName, userIcon;
+    // if user sent message directly to channel, too bad. get his info
+    if (message.user) {
+      var userProfile = slack.getUserByID(message.user);
+      userName = userProfile.name;
+      userIcon = userProfile.profile.image_48;
+    }
+    // get bot info
+    else if (message.username) {
+      userName = message.username;
+      userIcon = message.icons.image_48;
+    }
+
+    // open a DM channel and send user a message
+    console.log(message);
+    slack.openDM(user, function(dmChannel) {
+      // console.log(dmChannel);
+      var dm = slack.getDMByID(dmChannel.channel.id);
+      dm.postMessage({
+        text: message.text,
+        username: userName,
+        icon_url: userIcon
+      });
+      dm.close();
+    });
+  });
+}
+
+function onMessageFromDM (message) {
+  // do not listen to bots
+  if (message.subtype === 'bot_message') {
+    return;
+  }
+
+  function randomUsername () {
+    return Math.random().toString(36).substring(2);
+  }
+
+  function randomHexColor() {
+    return Math.random().toString(16).substring(2, 2+6);
+  }
+
+  var text = message.text;
+  var anonymousUser = randomUsername();
+  var someColor = randomHexColor();
+  var anonymousIcon = 'http://placehold.it/80/' + someColor + '/'+ someColor;
+
+  var response = {
+    text: text,
+    username: anonymousUser,
+    icon_url: anonymousIcon
+  };
+
+  // annotate who sent the message so the message isn't sent when broadcasting to users.
+  userSecretUsername[message.user] = anonymousUser;
+  userActivityOnChannel[message.user] = Date.now();
+
+  // post message to #anonymous
+  var anonymousChannel = slack.getChannelByName('anonymous');
+  anonymousChannel.postMessage(response);
+}
+
 slack.on('message', function (message) {
-  // if message is from myself, ignore
+  // console.log('message', message);
+  // TODO: if message is from myself, ignore
 
   var channelGroupOrDM = slack.getChannelGroupOrDMByID(message.channel);
-  var text = message.text;
-
-  // if message is a DM from a user...
-  if (channelGroupOrDM.is_im) {
-    var anonymousChannel = slack.getChannelByName('anonymous');
-    var anonymousUser = randomUsername();
-    var someColor = randomHexColor();
-    var anonymousIcon = 'http://placehold.it/80/' + someColor + '/'+ someColor;
-
-    var response = {
-      text: text,
-      username: anonymousUser,
-      icon_url: anonymousIcon
-    };
-
-    // post message to #anonymous
-    anonymousChannel.postMessage(response);
+  if( channelGroupOrDM.is_channel ) {
+    onMessageFromChannel(message);
+  } else if ( channelGroupOrDM.is_im ) {
+    onMessageFromDM(message);
   }
 });
 
@@ -84,11 +163,3 @@ slack.on('error', function (error) {
 });
 
 slack.login();
-
-function randomUsername () {
-  return Math.random().toString(36).substring(2);
-}
-
-function randomHexColor() {
-  return Math.random().toString(16).substring(2, 2+6);
-}
