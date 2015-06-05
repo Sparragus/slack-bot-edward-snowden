4daa89850ae1f5a293b9b61606b7c52267cf502c
Richard Kaufman <richardbkaufman@gmail.com>
2015-06-05 14:07:55 -0500

    HTTP server.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var Slack = require('slack-client');
+var http = require('http');
 
 var token = process.env.SLACK_API_TOKEN;
 var autoReconnect = true;
@@ -52,3 +53,22 @@ slack.on('error', function (error) {
 });
 
 slack.login();
+
+// I don't want this app to crash in case someone sends an HTTP request, so lets implement a simple server
+//Lets define a port we want to listen to
+const PORT = process.env.PORT || 3000;
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    var quote = "The NSA has built an infrastructure that allows it to intercept almost everything. With this capability, the vast majority of human communications are automatically ingested without targeting. If I wanted to see your emails or your wife's phone, all I have to do is use intercepts. I can get your emails, passwords, phone records, credit cards. I don't want to live in a society that does these sort of things... I do not want to live in a world where everything I do and say is recorded. That is not something I am willing to support or live under.\n\n-Edward Snowden, NSA files source: 'If they want to get you, in time they will', The Guardian, 10 June 2013.";
+    response.end(quote);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log('Server listening on: http://localhost:%s', PORT);
+});
